73cd1abeff10473455abb22ca558551a292f5e6f
raditz2488 <rohanbhale@gmail.com>
2018-01-19 00:15:20 +0530

    Code to add attachments to local notifications

---
+++ Remind/Remind/Model/NotificationAttachmentID.swift
@@ -0,0 +1,15 @@
+//
+//  NotificationAttachmentID.swift
+//  Remind
+//
+//  Created by pranav on 18/01/18.
+//  Copyright © 2018 RB. All rights reserved.
+//
+
+import Foundation
+
+enum NotificationAttachmentID: String {
+    case timer = "userNotification.attachment.timer"
+    case date = "userNotification.attachment.date"
+    case location = "userNotification.attachment.location"
+}
