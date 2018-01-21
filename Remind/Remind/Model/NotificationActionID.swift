6d73fa92a920e00f03de1fdf939c917fa9d2b89e
raditz2488 <rohanbhale@gmail.com>
2018-01-21 11:23:12 +0530

    Added actions and categories to local notifications

---
+++ Remind/Remind/Model/NotificationActionID.swift
@@ -0,0 +1,15 @@
+//
+//  NotificationActionID.swift
+//  Remind
+//
+//  Created by pranav on 20/01/18.
+//  Copyright © 2018 RB. All rights reserved.
+//
+
+import Foundation
+
+enum NotificationActionID: String {
+    case timer = "userNotification.action.timer"
+    case date = "userNotification.action.date"
+    case location = "userNotification.action.location"
+}
